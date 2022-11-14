a7b2f50ddbe66d1e2f05bca31ac7896a73142c00
Matthew Chang <mattchang908@gmail.com>
2022-11-14 00:50:28 -0600

    bare login screen

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Olympus</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+            <Text style={{textAlign:"center"}}>Olympus</Text>
+            <View>
+                <TextInput
+                    style={styles.ep}
+                    defaultValue="Email"
+                />
+                <TextInput
+                    style={styles.ep}
+                    defaultValue="Password"
+                />
+                <Button title={"Log in"}/>
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+    ep: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        textAlign: "center",
+        marginBottom: 5,
+    }
+
+
 });
