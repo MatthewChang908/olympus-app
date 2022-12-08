555768745907c3ae1454cc55d4b68fee5165501c
Matthew Chang <mattchang908@gmail.com>
2022-12-08 01:00:29 -0600

    new screens added, old login screen updated

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
 import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +12,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen options={{headerShown: false}} name={"Login"} component={LoginScreen}/>
+                <Stack.Screen options={{headerShown: false}} name={"Signup"} component={SignupScreen}/>
                 <Stack.Screen name={"Home"} component={HomeScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
---
+++ screens/SignupScreen.js
@@ -0,0 +1,151 @@
+import React, {useEffect, useState} from 'react'
+import {KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image} from 'react-native'
+import {auth} from "../firebase";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
+import {useNavigation} from "@react-navigation/core";
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
+
+const SignupScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            if (user) {
+                navigation.replace("Home")
+            }
+        })
+        return unsubscribe
+    }, [])
+
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword(auth, email,password)
+        .then((userCredentials) => {
+            const user = userCredentials.user;
+            console.log(user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+    const handleLogin = () => {
+        signInWithEmailAndPassword(auth, email,password)
+        .then((userCredentials) => {
+            const user = userCredentials.user;
+            console.log("Logged in with: ", user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+    return (
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior="padding"
+        >
+            <Image source={require('./olympus_logo.png')} />
+            <View style={styles.inputContainer}>
+                <Text style={styles.inputText}>Name</Text>
+                <TextInput
+                    selectionColor={'#E6C466'}
+                    style={styles.input}
+                />
+                <Text style={styles.inputText}>Email</Text>
+                <TextInput
+                    selectionColor={'#E6C466'}
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                />
+                <Text style={styles.inputText}>Password</Text>
+                <TextInput
+                    selectionColor={'#E6C466'}
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                />
+                <Text style={styles.inputText}>Phone</Text>
+                 <TextInput
+                    selectionColor={'#E6C466'}
+                    style={styles.input}
+                    keyboardType = 'numeric'
+                />
+                
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleSignUp}
+                    style={[styles.loginButton, styles.signUpButton]}>
+                    <Text style={styles.signUpText}>Join OLYMPUS</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
+    )
+}
+
+export default SignupScreen
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    olympusText:{
+        color: "white",
+        fontSize: "48px",
+        fontWeight: "bold",
+    },
+    inputContainer:{
+        width: '90%'
+    },
+    input: {
+        color: "#E6C466",
+        paddingHorizontal: 0,
+        paddingVertical: 0,
+        marginTop: 5,
+        borderBottomColor: 'white',
+        borderBottomWidth: 2,
+        fontSize: 20,
+    },
+    inputText: {
+        color: "white",
+        paddingBottom: 0,
+        paddingTop: 20,
+        fontSize: 20,
+    },
+    buttonContainer: {
+        width:'80%',
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+    },
+    loginButton: {
+        backgroundColor: "black",
+        width: '100%',
+        padding: 0,
+        borderRadius: 10,
+        alignItems: "center",
+        height: 48,
+        borderColor: "#E6C466",
+        borderWidth: 2
+    },
+    loginText: {
+        color: "white"
+    },
+    signUpButton: {
+        backgroundColor: "#E6C466",
+        marginTop: 5,
+        borderWidth: 2,
+        padding: 0,
+        justifyContent: 'center',
+    },
+    signUpText: {
+        color: "black",
+        fontSize: 20,
+    }
+
+
+})
