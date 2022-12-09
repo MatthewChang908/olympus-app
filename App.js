61f940a360dff89d4e8c6dd252b1bad4ffb22505
Matthew Chang <mattchang908@gmail.com>
2022-12-08 20:02:01 -0600

    welcome screen routes added

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import SignupScreen from "./screens/SignupScreen";
 import HomeScreen from "./screens/HomeScreen";
+import WelcomeScreen from './screens/WelcomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName="Welcome">
+                <Stack.Screen options={{headerShown: false}} name={"Welcome"} component={WelcomeScreen}/>
                 <Stack.Screen options={{headerShown: false}} name={"Signup"} component={SignupScreen}/>
                 <Stack.Screen name={"Home"} component={HomeScreen}/>
+                <Stack.Screen options={{headerShown: false}} name={"Login"} component={LoginScreen}/>
+
             </Stack.Navigator>
         </NavigationContainer>
     );
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native'
 import React from 'react'
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({navigation}) => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View
+        style={styles.container}
+    >
+      <Image style={styles.logo} source={require('./fading_logo.png')} />
+      
+
+      <View style={styles.buttonContainer}>
+          <TouchableOpacity
+              onPress={() => {navigation.navigate('Login')}}
+              style={[styles.loginButton]}>
+              <Text style={styles.loginText}>Log In</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+              onPress={() => {navigation.navigate('Signup')}}
+              style={[styles.loginButton, styles.signUpButton]}>              
+              <Text style={styles.signUpText}>Join OLYMPUS</Text>
+          </TouchableOpacity>
+      </View>
     </View>
   )
 }
-
+// 
 export default WelcomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    logo: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 250,
+    },  
+    buttonContainer: {
+        width:'90%',
+        marginBottom: 100,
+    },
+    loginButton: {
+        backgroundColor: "black",
+        width: '100%',
+        padding: 0,
+        borderRadius: 12,
+        alignItems: "center",
+        height: 48,
+        borderColor: "#E6C466",
+        borderWidth: 2,
+        justifyContent: 'center',
+    },
+    loginText: {
+        color: "white",
+        fontSize: 20,
+    },
+    signUpButton: {
+        backgroundColor: "#E6C466",
+        marginTop: 20,
+        
+    },
+    signUpText: {
+        color: "black",
+        fontSize: 20,
+    }
+})
